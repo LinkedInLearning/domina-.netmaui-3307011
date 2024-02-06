86f557e488976eb258de95714c0b9d7abc109cf0
Rodrigo Díaz Concha <rdiazconcha@hotmail.com>
2024-02-06 00:54:10 -0600

    01_07f

--- src/WisdomPetMedicine/MauiProgram.cs
+++ src/WisdomPetMedicine/MauiProgram.cs
@@ -13,7 +13,8 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+                fonts.AddFont("MaterialIcons-Regular.ttf", "MaterialIcons");
+            });
 
 		builder.ConfigureWisdomPetMedicine();
         return builder.Build();
