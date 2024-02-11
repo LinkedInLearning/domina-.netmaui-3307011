b4bc3a09a38bde84d3128287586d5b00e4002acb
Rodrigo Díaz Concha <rdiazconcha@hotmail.com>
2024-02-11 00:10:07 -0600

    02_08f

--- src/WisdomPetMedicine/MauiProgram.cs
+++ src/WisdomPetMedicine/MauiProgram.cs
@@ -1,6 +1,8 @@
 ﻿using WisdomPetMedicine.Extensions;
 using CommunityToolkit.Maui;
 using ZXing.Net.Maui.Controls;
+using CommunityToolkit.Maui.Maps;
+using Microsoft.Extensions.Configuration;
 
 namespace WisdomPetMedicine;
 public static class MauiProgram
@@ -8,6 +10,7 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
+        builder.Configuration.AddEnvironmentVariables();
         builder.UseMauiApp<App>().ConfigureFonts(fonts =>
         {
             fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
@@ -15,7 +18,8 @@ public static class MauiProgram
             fonts.AddFont("MaterialIcons-Regular.ttf", "MaterialIcons");
         }).UseMauiCommunityToolkit()
           .UseBarcodeReader()
-          .UseMauiMaps();
+          .UseMauiMaps()
+          .UseMauiCommunityToolkitMaps(builder.Configuration["BING_API_KEY"]);
         builder.ConfigureWisdomPetMedicine();
         return builder.Build();
     }
